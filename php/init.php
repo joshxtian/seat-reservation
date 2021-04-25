5cd11e0e2678db5b5d4b6c90ad2d828238731808
joshxtian <jaysaturno98@gmail.com>
2021-04-25 15:19:09 +0800

    change config

--- php/init.php
+++ php/init.php
@@ -1,6 +1,6 @@
 <?php
   require_once "function.php";
   spl_autoload_register(function($class){
-     require_once $_SERVER['DOCUMENT_ROOT']."/movie-reservation/class/".$class.'.php';
+     require_once $_SERVER['DOCUMENT_ROOT']."/class/".$class.'.php';
   });
 ?>
